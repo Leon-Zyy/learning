545e63796b4bc7ffece5abe4adffc68afe0561e5
zhaoyiyun <zhaoyiyun@meicai.cn>
2020-09-14 14:13:15 +0800

    添加类

---
+++ src/learn/algorithm/leetcodeSolution/LeetCodeQuestion226.java
@@ -0,0 +1,54 @@
+package learn.algorithm.leetcodeSolution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description leetCode问题
+ * @Author Leon.Zhao
+ * @Date 2020/3/10 16:25
+ * @Version 1.0
+ */
+public class LeetCodeQuestion226 {
+
+    /**
+     * 翻转一棵二叉树。
+     *
+     * 示例：
+     *
+     * 输入：
+     *
+     *      4
+     *    /   \
+     *   2     7
+     *  / \   / \
+     * 1   3 6   9
+     * 输出：
+     *
+     *      4
+     *    /   \
+     *   7     2
+     *  / \   / \
+     * 9   6 3   1
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode(int x) { val = x; }
+    }
+}
---
+++ src/learn/io/nio/Server.java
@@ -0,0 +1,123 @@
+package learn.io.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 服务端
+ */
+public class Server {
+
+    private Selector selector;
+    private ByteBuffer readBuffer = ByteBuffer.allocate(1024);//缓存大小
+    private ByteBuffer sendBuffer = ByteBuffer.allocate(1024);//缓存大小
+
+    String str;
+    public void start() throws IOException {
+        // 打开服务套接字通道
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        // 设置服务器配置为非阻塞
+        ssc.configureBlocking(false);
+        // 进行服务绑定
+        ssc.bind(new InetSocketAddress("localhost", 8001));
+
+        // 通过open()方法找到Selector
+        selector = Selector.open();
+        // 注册到selector，等待连接
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+        while (!Thread.currentThread().isInterrupted()){
+            selector.select();
+            Set<SelectionKey> keys = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = keys.iterator();
+            while (keyIterator.hasNext()) {
+                SelectionKey key = keyIterator.next();
+                if (!key.isValid()) {
+                    continue;
+                }
+                if (key.isAcceptable()) {
+                    accept(key); // 接受了一个连接
+                } else if (key.isConnectable()) {
+                    // 与服务器建立了连接
+                }else if(key.isReadable()) {
+                    read(key); // 一个通道已读就绪
+                } else if (key.isWritable()) {
+                    write(key); // 一个通道已写就绪
+                }
+
+                keyIterator.remove();// 该事件已经处理完毕，可以丢弃
+            }
+        }
+    }
+
+    /**
+     * 写操作
+     *
+     * @param key
+     * @throws IOException
+     * @throws ClosedChannelException
+     */
+    private void write(SelectionKey key) throws IOException, ClosedChannelException {
+        SocketChannel channel = (SocketChannel) key.channel();
+        System.out.println("write:"+str);
+
+        sendBuffer.clear();
+        sendBuffer.put(str.getBytes());
+        sendBuffer.flip();
+        channel.write(sendBuffer);
+        channel.register(selector, SelectionKey.OP_READ);
+    }
+
+    /**
+     * 读操作
+     *
+     * @param key
+     * @throws IOException
+     */
+    private void read(SelectionKey key) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+
+        // Clear out our read buffer so it's ready for new data
+        this.readBuffer.clear();
+//        readBuffer.flip();
+        // Attempt to read off the channel
+        int numRead;
+        try {
+            numRead = socketChannel.read(this.readBuffer);
+        } catch (IOException e) {
+            // The remote forcibly closed the connection, cancel
+            // the selection key and close the channel.
+            key.cancel();
+            socketChannel.close();
+
+            return;
+        }
+
+        str = new String(readBuffer.array(), 0, numRead);
+        System.out.println(str);
+        socketChannel.register(selector, SelectionKey.OP_WRITE);
+    }
+
+    /**
+     * 接受连接
+     *
+     * @param key
+     * @throws IOException
+     */
+    private void accept(SelectionKey key) throws IOException {
+        ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+        SocketChannel clientChannel = ssc.accept();
+        clientChannel.configureBlocking(false);
+        clientChannel.register(selector, SelectionKey.OP_READ);
+        System.out.println("a new client connected "+clientChannel.getRemoteAddress());
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("server started...");
+        new Server().start();
+    }
+}
